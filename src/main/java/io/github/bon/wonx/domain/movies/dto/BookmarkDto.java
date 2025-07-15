a5bb6c67647d3481fcbc7f882d07979a7a819823
Minyong Kim <ravenade88@gmail.com>
2025-07-15 17:13:32 +0900

    update: bookmark, like API 응답 형식 수정

--- src/main/java/io/github/bon/wonx/domain/movies/dto/BookmarkDto.java
+++ src/main/java/io/github/bon/wonx/domain/movies/dto/BookmarkDto.java
@@ -1,6 +1,5 @@
 package io.github.bon.wonx.domain.movies.dto;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 import io.github.bon.wonx.domain.movies.entity.Bookmark;
@@ -12,29 +11,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookmarkDto {
-    private UUID id;
     private UUID userId;
     private UUID movieId;
-    private LocalDateTime createdAt;
     private boolean isBookmarked;
 
     public static BookmarkDto from(Bookmark bookmark) {
         return new BookmarkDto(
-            bookmark.getId(),
             bookmark.getUser().getId(),
             bookmark.getMovie().getId(),
-            bookmark.getCreatedAt(),
             true
         );
     }
 
     public static BookmarkDto notBookmarked(UUID userId, UUID movieId) {
-        return new BookmarkDto(
-            null,
-            userId,
-            movieId,
-            null,
-            false
-        );
+        return new BookmarkDto(userId, movieId, false);
     }
 }
--- src/main/java/io/github/bon/wonx/domain/movies/dto/LikeDto.java
+++ src/main/java/io/github/bon/wonx/domain/movies/dto/LikeDto.java
@@ -1,6 +1,5 @@
 package io.github.bon.wonx.domain.movies.dto;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 import io.github.bon.wonx.domain.movies.entity.Like;
@@ -12,30 +11,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LikeDto {
-    private UUID id;
     private UUID userId;
     private UUID movieId;
-    private LocalDateTime createdAt;
     private boolean isLiked;
     
     public static LikeDto from(Like like) {
         return new LikeDto(
-            like.getId(),
             like.getUser().getId(),
             like.getMovie().getId(),
-            like.getCreatedAt(),
             true
         );
     }
 
     public static LikeDto notLiked(UUID userId, UUID movieId) {
-        return new LikeDto(
-            null,
-            userId,
-            movieId,
-            null,
-            false
-        );
+        return new LikeDto(userId, movieId, false);
     }
 }
 
